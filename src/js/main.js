2f66ed68b43536cd57684d3349b2d325bd891383
Maxim <xbultazar@gmail.com>
2021-03-03 18:39:38 +0300

    add toggle

--- src/js/main.js
+++ src/js/main.js
@@ -15,11 +15,11 @@ import $ from './lib/lib';
 
 // console.log($('button').html('hello'));
 $('#first').on('click', ()=> {
-    $('div').eq(1).fadeOut(800);
+    $('div').eq(1).fadeToggle(800);
 });
 $('[data-count="second"]').on('click', ()=> {
-    $('div').eq(2).fadeOut(800);
+    $('div').eq(2).fadeToggle(800);
 });
 $('button').eq(2).on('click', () => {
-    $('.w-500').fadeOut(800);
+    $('.w-500').fadeToggle(800);
 });
