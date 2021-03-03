130013d9f94f2399b0b3f78dbceca0bbd8dffd3f
Maxim <xbultazar@gmail.com>
2021-03-03 14:06:47 +0300

    add styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,25 @@
 import $ from './lib/lib';
 
-$('button').on('click', function() {
-    $('div').eq(2).toggleClass('active');
-});
+// $('button').on('click', function() {
+//     $('button').toggleClass('active');
+// });
 
-$('div').click(function() {
-    console.log($(this).index());
-});
+// $('div').click(function() {
+//     console.log($(this).index());
+// });
 
 // console.log($('div').eq(2).find('.more'));
 // console.log($('.some').closest('.findme'));
-$('.findme').fadeOut(1800);
+// $('button').fadeOut(1800);
+// $('button').fadeIn(1800);
 
 // console.log($('button').html('hello'));
+$('#first').on('click', ()=> {
+    $('div').eq(1).fadeOut(800);
+});
+$('[data-count="second"]').on('click', ()=> {
+    $('div').eq(2).fadeOut(800);
+});
+$('button').eq(2).on('click', () => {
+    $('.w-500').fadeOut(800);
+});
